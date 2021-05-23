6146f76a55621a3f87b831828f513753ee7cf3e2
Svavar <spg009@gmail.com>
2021-05-23 18:01:11 +0000

    adding the update an item controller

--- src/Controller/listController.ts
+++ src/Controller/listController.ts
@@ -1,11 +1,23 @@
 // here we are going to place the controllers
 import todoListModel, { itemsModel } from "../Model/listModel";
 
+/**
+ * todo list controllers
+ */
 const todolistController = {
+	/**
+	 * Returns all lists
+	 * @returns JSON
+	 */
 	getAllLists() {
 		return todoListModel.find() || [];
 	},
-
+	/**
+	 * Creates a new item within a list
+	 * @param item String
+	 * @param listId String
+	 * @returns JSON
+	 */
 	async postNewItem(item: string, listId: string) {
 		interface Iitems {
 			item: string;
@@ -25,13 +37,32 @@ const todolistController = {
 		return listToSaveToo;
 	},
 
+	/**
+	 * Returns a list by ID
+	 * @param listId String
+	 * @returns JSON
+	 */
 	getListById(listId: string) {
 		return todoListModel.findById(listId) || [];
 	},
 
-	async updateItemInList() {},
+	/**
+	 * Updates a item within a list
+	 * @param updatedItem String
+	 * @param itemId String
+	 * @returns mongoDB Query
+	 */
+	async updateItemInList(updatedItem: string, itemId: string) {
+		return itemsModel.findByIdAndUpdate(itemId, { item: updatedItem });
+	},
 
-	async updateList() {},
+	/**
+	 * Updates a specifics lists name
+	 * @param listId string
+	 * @param newName string
+	 * @returns mongoDB Query
+	 */
+	async updateListName(listId: string, newName: string) {},
 
 	deleteItem() {},
 
